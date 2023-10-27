64959ca8579778c0d9e51548fa320148a2101a6a
GuillermoJesus <67805272+GuillermoJesus@users.noreply.github.com>
2023-10-26 22:31:14 -0300

    Test1 IBM DataScience
    Se agrega primer archivo requerido para prueba IBM

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the key takeaways from the course
+# 
+# **Objectives:**
+# * Popular Lenguajes in DS
+# * Common Libraries used in DS
+# * Examples of arithmetics expression in Python
+# 
+#     
+#     
+
+# Some of the popular languages that Data Scientists use are:
+# * Python
+# * R
+# * SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# Data Science Tools |
+# --- |
+# Pandas |
+# NumPy |
+# Scikit-learn |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# a = 5
+# b = 3
+# suma = a + b
+# print(suma)
+# 
+# 
+# c = 1
+# d = 34
+# resta = x - y
+# print(resta)
+# 
+# e = 9
+# f = 2
+# multiplicacion = p * q
+# print(multiplicacion)
+# 
+
+# In[4]:
+
+
+(3*4)+5
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[5]:
+
+
+segundos = 200
+horas = segundos / 3600
+print(horas)
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# ## Author
+# Guillermo Flores
+
+# In[ ]:
+
+
+
+
